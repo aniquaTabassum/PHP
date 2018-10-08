24215d4f0f68c695b3ea3d6fd5c0345cb9ac64a8
Aniqua Tabassum <32939382+aniquaTabassum@users.noreply.github.com>
2018-10-08 11:52:07 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+	<title> PHP IS WEIRD </title>
+	<style>
+	.phpcoding{width:900px; margin: 0 auto; background:#ddd; padding 40px;}
+	.headersetting, .footeroption{background:#444; color:#fff; text-align: center;}
+	.phppart{min-height:40px;background:#fff;color:#444}
+	.headersetting h2, .footeroption h2{margin:0}
+	</style>
+</head>
+<body>
+
+	<div class = "phpcoding">
+		<section class = "headersetting">
+			
+			<h2> this is supposed to be some sort of header</h2>
+		<section>
+		
+		<section class = "phppart">
+			<?php
+			    echo "eto kichu install korte keno hobe?";
+			?>
+		</section>
+		
+		<section class = "footeroption">
+			
+			<h2> <?php echo "this is a footer I guess?";?></h2>
+		</section>
+	</div>
+</body>
+</html>
